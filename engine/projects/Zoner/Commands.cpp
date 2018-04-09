9026d1fe3cc7fff15e4a3612721a3a07a61f6a49
Tony Forge <antonka@playrix.com>
2018-04-10 00:58:26 +0300

    Zoner update.

--- engine/projects/Zoner/Commands.cpp
+++ engine/projects/Zoner/Commands.cpp
@@ -1,10 +1,39 @@
 #include "Commands.h"
 
+std::vector<Zoner::CommandsList*> Zoner::CommandsList::all_lists;
+size_t alive_lists = 0;
+
 std::vector<std::pair<int, Zoner::ICommand*>> Zoner::CommandsList::_commands_cache;
 
 Zoner::CommandsList::CommandsList()
 {
+	all_lists.push_back(this);
+	this_in_all_lists_index = all_lists.size() - 1;
+
+	alive_lists++;
+
 	_commands_shift = 0;
+
+	_commands_realtime_60_timer = 0.f;
+	_commands_realtime_30_timer = 0.f;
+
+	_commands_daily_240_timer = 0.f;
+	_commands_daily_96_timer = 0.f;
+	_commands_daily_48_timer = 0.f;
+	_commands_daily_4_timer = 0.f;
+	_commands_daily_1_timer = 0.f;
+
+	dt_accumulator = 0;
+	hours_accumulator = 0;
+
+	realtime_frames_accumulator = 0;
+	gametime_frames_accumulator = 0;
+}
+
+Zoner::CommandsList::~CommandsList()
+{
+	all_lists[this_in_all_lists_index] = nullptr;
+	alive_lists--;
 }
 
 void Zoner::CommandsList::Push(Zoner::ICommand * cmd, int group_id)
@@ -209,6 +238,145 @@ void Zoner::CommandsList::OnNewDay()
 		}
 
 		i++;
+	}
+}
+
+void Zoner::CommandsList::Update(float dt, float hours_passed, bool new_day)
+{
+	//if (owner is inside current space)
+	//{
+	_commands_realtime_60_timer += dt;
+	_commands_realtime_30_timer += dt;
+
+	if (_commands_realtime_60_timer >= 0.01666667f)
+	{
+		//if owner is in viewport
+		for (auto&& cmd : _commands_realtime_60)
+		{
+			//cmd.Execute
+		}
+
+		_commands_realtime_60_timer = 0.f;
+	}
+
+	if (_commands_realtime_30_timer >= 0.03333333f)
+	{
+		//if (owner is not in viewport)
+		/*{
+		for (auto&& cmd : _commands_realtime_60)
+		{
+		//cmd.Execute
+		}
+		}*/
+
+		for (auto&& cmd : _commands_realtime_30)
+		{
+			//cmd.Execute
+		}
+
+		_commands_realtime_30_timer = 0.f;
+	}
+
+	//}
+
+	_commands_daily_240_timer += hours_passed;
+	_commands_daily_96_timer += hours_passed;
+	_commands_daily_48_timer += hours_passed;
+	_commands_daily_4_timer += hours_passed;
+	_commands_daily_1_timer += hours_passed;
+
+	if (_commands_daily_240_timer >= 0.1f)
+	{
+		for (auto&& cmd : _commands_daily_240)
+		{
+			//cmd.Execute
+		}
+
+		_commands_daily_240_timer = 0.f;
+	}
+
+	if (_commands_daily_96_timer >= 0.25f)
+	{
+		//if owner is inside current space
+		for (auto&& cmd : _commands_daily_96)
+		{
+			//cmd.Execute
+		}
+
+		_commands_daily_96_timer = 0.f;
+	}
+
+	if (_commands_daily_48_timer >= 0.5f)
+	{
+		//if owner is not inside current space
+		/*for (auto&& cmd : _commands_daily_96)
+		{
+			//cmd.Execute
+		}*/
+
+		for (auto&& cmd : _commands_daily_48)
+		{
+			//cmd.Execute
+		}
+
+		_commands_daily_48_timer = 0.f;
+	}
+
+	if (_commands_daily_4_timer >= 6.0f)
+	{
+		for (auto&& cmd : _commands_daily_4)
+		{
+			//cmd.Execute
+		}
+
+		_commands_daily_4_timer = 0.f;
+	}
+
+	if (_commands_daily_1_timer >= 24.0f)
+	{
+		for (auto&& cmd : _commands_daily_1)
+		{
+			//cmd.Execute
+		}
+
+		_commands_daily_1_timer = 0.f;
+	}
+}
+
+void Zoner::CommandsList::AllListsUpdate(float dt, float hours_passed, bool new_day)
+{
+	bool repack_needed = false;
+
+	if (alive_lists == 0) all_lists.clear();
+
+	if (all_lists.size() > 0)
+	{
+		if (static_cast<float>(all_lists.size() - alive_lists) / static_cast<float>(all_lists.size()) > 0.25f) repack_needed = true;
+
+		if (repack_needed)
+		{
+			std::vector<Zoner::CommandsList*> all_lists_temp;
+			all_lists_temp.reserve(alive_lists);
+
+			for (auto&& cmd_list : all_lists)
+			{
+				if (cmd_list != nullptr)
+				{
+					all_lists_temp.push_back(cmd_list);
+					all_lists_temp[all_lists_temp.size() - 1]->this_in_all_lists_index = all_lists_temp.size() - 1;
+				}
+			}
+
+			all_lists = all_lists_temp;
+		}
+
+		for (auto&& cmd_list : all_lists)
+		{
+			if (cmd_list != nullptr)
+			{
+				cmd_list->Update(dt, hours_passed, new_day);
+			}
+		}
 	}
 }
 
--- engine/projects/Zoner/Commands.h
+++ engine/projects/Zoner/Commands.h
@@ -28,6 +28,7 @@ namespace Zoner
 	{
 	public:
 		CommandsList();
+		~CommandsList();
 
 		void Push(Zoner::ICommand* cmd, int group_id);
 		void Remove(int group_id);
@@ -41,10 +42,44 @@ namespace Zoner
 		void ApplyPassedTime();
 		void OnNewDay();
 
+		void Update(float dt, float hours_passed, bool new_day); //must be called as fast as possible (realtime, 60 fps limit inside)
+
+		size_t this_in_all_lists_index;
+		float dt_accumulator;
+		float hours_accumulator;
+		int realtime_frames_accumulator;
+		int gametime_frames_accumulator;
+
+		static void AllListsUpdate(float dt, float hours_passed, bool new_day);
+		static std::vector<Zoner::CommandsList*> all_lists;
+		static size_t alive_lists;
 	protected:
 	private:
 		size_t _commands_shift;
 		std::vector<std::pair<int, Zoner::ICommand*>> _commands;
+
+		//realtime commands execute happens only in current space!
+		std::vector<std::pair<int, Zoner::ICommand*>> _commands_realtime_60; //for smooth and slow animations in viewport
+		std::vector<std::pair<int, Zoner::ICommand*>> _commands_realtime_30; //for rough and fast animations in viewport, and for all animations outside of viewport
+
+		std::vector<std::pair<int, Zoner::ICommand*>> _commands_daily_240; //realtime, 60 per real second, 240 times per 24 game hours which is 4 real seconds (animations, movements)
+		std::vector<std::pair<int, Zoner::ICommand*>> _commands_daily_96; //24 times per real second, 96 times per game day (most of ai logic here)
+		std::vector<std::pair<int, Zoner::ICommand*>> _commands_daily_48; //12 times per real second, 48 times per game day (ai logic at spaces that is not current)
+		std::vector<std::pair<int, Zoner::ICommand*>> _commands_daily_4; //1 per real second, 4 per game day, every 6 game hours (some very rare ai logic)
+		std::vector<std::pair<int, Zoner::ICommand*>> _commands_daily_1; //1 per game day, executes at the beginning of the game day
+
+		//call priority ((((240) 96) 48) 4) 1
+		//timers
+		float _commands_realtime_60_timer;
+		float _commands_realtime_30_timer;
+
+		float _commands_daily_240_timer;
+		float _commands_daily_96_timer;
+		float _commands_daily_48_timer;
+		float _commands_daily_4_timer;
+		float _commands_daily_1_timer;
+
+		std::vector<std::pair<int, Zoner::ICommand*>> _commands_fps_;
 		static std::vector<std::pair<int,Zoner::ICommand*>> _commands_cache;
 	};
 }
